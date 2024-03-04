9b429fb9f96dbae905de21f7dcf1a2722a86c7fa
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-03-04 20:02:13 -0300

    adicionei "Vetor1"

---
+++ vetor1/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] coisas = new int[10]; // declara um vetor de inteiros
+        String[] palavras = {"aaa", "bbb", "ccc", "ddd", "eee"};
+
+        for (int i = 0; i < coisas.length; i++) {
+            System.out.println(i + " - " + coisas[i]);
+        }
+
+        // vasculhe "palavras" e coloque cada elemento
+        // na variável "x" uma volta de cada vez
+        for (String x: palavras) {
+            System.out.println(x);
+        }
+    }
+}
