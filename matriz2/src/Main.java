e335597639af923faffbb4717751985bd8bd57cd
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-04-24 21:55:46 -0300

    Fiz umas coisa

--- matriz2/src/Main.java
+++ matriz2/src/Main.java
@@ -1,20 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        // vetor de dias da semana
+        String[] semana = {"seg","ter", "qua", "qui", "sex"};
 
-        // criar um vetor usando new
-        double[] valores = new double[7];
+        // agenda do consultório (4 consultas por dia, 5 dias por semana)
+        String[][] agenda = new String[5][4];
 
-        // também dá pra criar vetores de tamanho variável
-        int n = 11;
-        String[] palavras = new String[n];
+        // pedir o nome de um paciente
+            // repetir até o nome ser vazio (teclar ENTER no nome)
+            // pedir o dia da semana que quer consulta (3 letras)
+            // se o dia não existe, avisa e não faz nada
+            // se tiver horário livre, agendar, se não tiver, reclamar
+            // imprimir a agenda
+            /* exemplo de impressão da agenda
+            Seg: Maria
+            Ter:
+            Qua:
+            Qui: Juca, João
+            Sex:
+             */
 
-        // o atributo .length contém o tamanho de um vetor
-        for(int i=0; i < valores.length; i++){
-            valores[i] = 0;
-        }
-
-        // matrizes com mais dimensões são declaradas assim
-        int[][] horarios = new int[24][7];
 
     }
 }
--- vetor1/src/Main.java
+++ vetor1/src/Main.java
@@ -1,15 +1,59 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        int[] coisas = new int[10]; // declara um vetor de inteiros
-        String[] palavras = new String[10];
-        for (int i = 0; i < coisas.length; i++) {
-            System.out.println(i + " - " + coisas[i]);
+
+    public static void exercicio1(){
+        // criar um vetor de 10 inteiros
+        int[] vetor1 = new int[10];
+        int[] vetor2 = new int[10];
+
+        // encha com números aleatórios
+        Random random = new Random();
+        for (int i = 0; i < vetor1.length; i++) {
+            vetor1[i] = random.nextInt(100);
+            vetor2[9-i] = vetor1[i];
         }
 
-        // vasculhe "palavras" e coloque cada elemento
-        // na variável "x" uma volta de cada vez
-        for (String x: palavras) {
-            System.out.println(x);
+        // imprima a lista
+        System.out.println("Vetor original");
+        imprimeVetor(vetor1);
+
+        // imprime o inverso
+        System.out.println("Vetor invertido");
+        imprimeVetor(vetor2);
+    }
+
+    public static void imprimeVetor(int[] vetor){
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
         }
+    }
+
+    public static void exercicio2(){
+        // cria um vetor de 6 nomes
+        String[] vetor = {"Jo", "Ana", "Pindamonhangaba",
+                "Juarez","Hipopótamo","Marieta"};
+
+        int menor = 0;
+        int maior = 0;
+
+        // imprimir qual o mais comprido e qual o mais curto
+        for (int i = 0; i < vetor.length; i++) {
+            if(vetor[i].length() > vetor[maior].length()) maior = i;
+            if(vetor[i].length() < vetor[menor].length()) menor = i;
+        }
+
+        System.out.println("+comprido: "+vetor[maior]);
+        System.out.println("+curto   : "+vetor[menor]);
+
+        // pedir um nome (questão 5)
+
+        // dizer em que posição do vetor está (ou se não está)
+
+    }
+
+    public static void main(String[] args) {
+        exercicio1();
+        exercicio2();
     }
 }
